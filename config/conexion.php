350b8534aeba827cd438aac8c37cf85820052651
Sofia Alvarez Rodero <sofialvarezrodero97@gmail.com>
2024-04-07 11:42:03 -0300

    Reordenamiento de archivos

--- config/conexion.php
+++ config/conexion.php
@@ -1,14 +1,15 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "cuponera");
- 
+$mysqli = new mysqli("localhost", "root", "", "servicios");
+
     $host="localhost";
     $user="root";
     $pass="";
 
-    $bd="cuponera";
+    $bd="servicios";
 
     $con=mysqli_connect($host,$user,$pass);
+    mysqli_set_charset($con, "utf8");
 
     mysqli_select_db($con,$bd);
 
