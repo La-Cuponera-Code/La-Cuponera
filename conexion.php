8b48eb96634e1c7c1a9e8a4d8efd5e361ae5a920
Sofia Alvarez Rodero <sofialvarezrodero97@gmail.com>
2024-04-08 22:35:38 -0300

    Estilos y redaccion pages

--- conexion.php
+++ conexion.php
@@ -1,17 +1,20 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "cuponera");
+$mysqli = new mysqli("localhost", "root", "", "cuponera","3307");
  
     $host="localhost";
     $user="root";
     $pass="";
+    $port = "3307"; // Especifica el puerto correcto aquí
+
 
     $bd="cuponera";
 
-    $con=mysqli_connect($host,$user,$pass);
+    $con=mysqli_connect($host,$user,$pass,$port);
 
     mysqli_select_db($con,$bd);
 
     return $con;
 
 ?>
+ 
